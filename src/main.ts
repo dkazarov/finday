5bcb8002d63d29ffe833d0e436f66060007ec266
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-09-10 23:35:53 +0300

    feat: load budget from local storage

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { initializeIcons } from './icons/icons';
 import './styles/main.css';
 import { calculateBudget } from './services/budgetService';
 import { budgetInput } from './data/budgetData';
+import { saveBudget, getBudget } from './services/StorageService';
 
 initializeIcons();
 
-const budgetResult = calculateBudget(budgetInput);
+let savedBudget = getBudget();
 
-console.log(budgetResult);
-console.log(budgetResult.dailyAmount);
+if (!savedBudget) {
+  saveBudget(budgetInput);
+  savedBudget = budgetInput;
+}
+
+console.log('Збережений бюджет:', savedBudget);
+
+const budgetResult = calculateBudget(savedBudget);
+
+console.log('Результат:', budgetResult);
+console.log('Сьогодні можна витратити:', budgetResult.dailyAmount);
